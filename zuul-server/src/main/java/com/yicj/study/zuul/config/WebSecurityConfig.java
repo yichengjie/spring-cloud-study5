da7ab64c2ab69638ccb55c3d0937fb4c8654937e
易成杰 <626659321@qq.com>
2020-12-31 20:55:01 +0800

    保存

--- zuul-server/src/main/java/com/yicj/study/zuul/config/WebSecurityConfig.java
+++ zuul-server/src/main/java/com/yicj/study/zuul/config/WebSecurityConfig.java
@@ -3,9 +3,7 @@ package com.yicj.study.zuul.config;
 import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.oauth2.config.annotation.web.configuration.EnableOAuth2Client;
 
 //@EnableWebSecurity 这里可以不用添加
 @Configuration
@@ -14,7 +12,7 @@ public class WebSecurityConfig  extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-            .antMatchers("/login","/resource/**").permitAll()
+            .antMatchers("/login","/resource/**","/uua/**").permitAll()
             .anyRequest().authenticated().and()
             .csrf().disable() ;
     }
