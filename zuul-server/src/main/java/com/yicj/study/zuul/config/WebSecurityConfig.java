1a49dcf36252bab73a9336543a4ff8d6b5b26324
易成杰 <626659321@qq.com>
2020-12-31 13:47:46 +0800

    保存

--- zuul-server/src/main/java/com/yicj/study/zuul/config/WebSecurityConfig.java
+++ zuul-server/src/main/java/com/yicj/study/zuul/config/WebSecurityConfig.java
@@ -9,7 +9,6 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.E
 
 @EnableWebSecurity
 @EnableOAuth2Sso
-//@EnableOAuth2Client
 @Configuration
 public class WebSecurityConfig  extends WebSecurityConfigurerAdapter {
     @Override
