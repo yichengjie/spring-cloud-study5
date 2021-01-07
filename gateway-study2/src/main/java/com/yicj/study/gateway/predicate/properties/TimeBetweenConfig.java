4bca4a5e069ae0f5d52ba3b7e22ad80e18a75e8f
易成杰 <626659321@qq.com>
2021-01-07 15:35:15 +0800

    保存

---
+++ gateway-study2/src/main/java/com/yicj/study/gateway/predicate/properties/TimeBetweenConfig.java
@@ -0,0 +1,11 @@
+package com.yicj.study.gateway.predicate.properties;
+
+import lombok.Data;
+
+import java.time.LocalTime;
+
+@Data
+public class TimeBetweenConfig {
+    private LocalTime start ;
+    private LocalTime end ;
+}
