c89867c21e4e0ffac7d97a8edd1b8dbd8fd89a4a
yicj <626659321@qq.com>
2021-01-09 16:14:50 +0800

    路由测试

--- gateway-study2/src/main/java/com/yicj/study/gateway/config/MyConfig.java
+++ gateway-study2/src/main/java/com/yicj/study/gateway/config/MyConfig.java
@@ -1,4 +1,20 @@
 package com.yicj.study.gateway.config;
 
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class MyConfig {
+
+    //@Bean
+    public RouteLocator customRouteLocator(RouteLocatorBuilder builder){
+        return builder.routes()
+                .route("query_route", r->
+                        r.query("foo", "bar")
+                         .uri("http://www.jd.com"))
+                .build() ;
+
+    }
 }
