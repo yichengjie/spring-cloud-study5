f85180a95d2d015f4af99d7732afbfe2aaf4053f
易成杰 <626659321@qq.com>
2021-01-11 00:00:31 +0800

    保存

--- open-feign-study/src/main/java/com/yicj/study/feign/OpenFeignApplication.java
+++ open-feign-study/src/main/java/com/yicj/study/feign/OpenFeignApplication.java
@@ -1,6 +1,5 @@
 package com.yicj.study.feign;
 
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
