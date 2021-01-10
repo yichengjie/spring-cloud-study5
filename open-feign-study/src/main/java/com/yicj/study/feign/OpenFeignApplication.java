f7787658ee8bdd5fcee37f2d187202dada279024
yichengjie <626659321@qq.com>
2021-01-10 10:43:02 +0800

    feign

---
+++ open-feign-study/src/main/java/com/yicj/study/feign/OpenFeignApplication.java
@@ -0,0 +1,18 @@
+package com.yicj.study.feign;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@EnableFeignClients
+@SpringBootApplication
+public class OpenFeignApplication {
+
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(OpenFeignApplication.class, args) ;
+    }
+
+}
